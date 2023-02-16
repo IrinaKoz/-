28efc2de50533bf43fc8169a172722f134822316
Irina Evgen'evna <118754599+IrinaKoz@users.noreply.github.com>
2023-02-16 19:55:17 +0300

    Add files via upload

---
+++ телебот.py
@@ -0,0 +1,44 @@
+import requests
+import telebot
+import json
+
+
+TOKEN = '6044412530:AAF6GYRIyiUUlrhVH1ro0yc71H129C3T9zs'
+
+
+bot = telebot.TeleBot(TOKEN)
+
+
+keys = {
+    'биткоин': 'BTC',
+    'эфириум': 'ETH',
+    'доллар:': 'USD',
+}
+
+
+@bot.message_handler(commands=['start', 'help'])
+def help(message: telebot.types.Message):
+    text = 'Чтобы начать работу введите команду боту в следующем формате:\n<имя валюты> \
+<в какую валюту перевести> \
+<количество переводимой валюты>\nУвидеть список всех доступных валют: /values'
+    bot.reply_to(message, text)
+
+
+@bot.message_handler(commands=['values'])
+def values(message: telebot.types.Message):
+    text = 'Доступные валюты:'
+    for key in keys.keys():
+        text = '\n'.join((text, key, ))
+    bot.reply_to(message, text)
+
+
+@bot.message_handler(content_types=['text', ])
+def convert(message: telebot.types.Message):
+    quote, base, amount = message.text.split(' ')
+    r = requests.get(f'https://min-api.cryptocompare.com/data/price?fsym={keys[quote]}&tsyms={keys[base]}')
+    total_base = json.loads(r.content)[keys[base]]
+    text = f'Цена {amount} {quote} в {base} - {total_base}'
+    bot.send_message(message.chat.id, text)
+
+
+bot.polling()
